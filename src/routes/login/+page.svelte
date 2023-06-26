c63357157ac192c40199c09a2a569eb385b9678f
Johann six <six.johann@gmail.com>
2023-06-26 08:26:38 +0200

    ✨ create login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<svelte:head>
+    <title>Login</title>
+</svelte:head>
+
+<main>
+    <h1>Login</h1>
+    
+</main>
+
+<style lang="scss">
+    main {
+        padding: 1rem;
+    }
+
+</style>
