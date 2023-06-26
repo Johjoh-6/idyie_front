e78674db77a40e0b1428417dc7801f1316423087
Johann six <six.johann@gmail.com>
2023-06-26 19:42:02 +0200

    🚧 add register page and logic

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,37 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { API_URL } from '$env/static/private';
+
+
+
+export const load: PageServerLoad = ({ locals }) => {
+	if (locals.user) throw redirect(302, '/profile');
+};
+
+export const actions: Actions = {
+    default: async ({ cookies, request }) => {
+        const data = await request.formData();
+        const email = data.get('email') as string;
+        const password = data.get('password') as string;
+        const confirm = data.get('password_confirm') as string;
+
+        if(password !== confirm) return fail(400, { password, confirm, mismatch: 'Passwords do not match.' });
+
+        if(!email || !password) return fail(400, { email, password, missing: 'Email or password are missing.' });
+
+        const response = await fetch(API_URL + 'api/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+          });
+        
+        const authToken = response.headers.get('set-cookie');
+        if(!authToken) throw new Error('Login failed');
+        const cookieName = authToken.substring(0, authToken.indexOf('='));
+        const cookieValue = authToken.substring(authToken.indexOf('=') + 1, authToken.indexOf(';'));
+    
+         cookies.set(cookieName, cookieValue, { path: '/', httpOnly: true});
+
+        throw redirect(307, '/profile');
+    }
+};
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+	import type { PageData, ActionData } from './$types';
+
+	export let form: ActionData;
+</script>
+
+<section>
+    <h1>Crée un compte</h1>
+    <form method="POST" use:enhance>
+        {#if form?.missing}<p class="error">{form.missing}</p>{/if}
+        <label for="email">Email : </label>
+        <input name="email" type="email" placeholder="test@gmail.com"/>
+        <label for="password">Mot de passe : </label>
+        <input name="password" type="password" />
+        <label for="password_confirm">Confirmer le mots de passe : </label>
+        <input name="password_confirm" type="password" />
+        {#if form?.mismatch}<p class="error">{form.mismatch}</p>{/if}
+        <button>S'enregistrer</button>
+    </form>
+</section>
+
+<style lang="scss">
+    section{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+        width: 100%;
+
+
+        h1{
+            font-size: 3rem;
+            color: var(--c_primary);
+        }
+        form{
+            border-radius: var(--br_sm);
+            padding: 15px;
+            background-color: var(--box_light);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 15px;
+            width: clamp(300px, 50%, 600px);
+
+            label{
+                font-weight: bold;
+                color: var(--c_primary);
+            }
+            input{
+                width: 100%;
+                padding: 10px;
+                border-radius: var(--br_sm);
+                border: 1px solid var(--c_primary);
+            }
+
+            button{
+                width: 100%;
+                padding: 10px;
+                border-radius: var(--br_sm);
+                border: 1px solid var(--c_primary);
+                background-color: var(--c_primary);
+                color: var(--c_grey);
+                font-weight: bold;
+                cursor: pointer;
+                &:hover{
+                    background-color: var(--c_light);
+                    color: var(--c_primary);
+                }
+            }
+
+        }
+    }
+</style>
