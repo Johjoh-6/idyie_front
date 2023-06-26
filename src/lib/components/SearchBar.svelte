b8da4265c100b1b86f97f0bcac214ea4e79c7645
Johann six <six.johann@gmail.com>
2023-06-26 19:40:45 +0200

    🚧 on component header and tutorial

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,40 @@
+<!-- SearchBar.svelte -->
+<script>
+    import Icon from './Icon.svelte';
+  </script>
+  
+  <div class="search-container">
+    <Icon name="search" color={"--c_grey_dark"} />
+    <input type="text" placeholder="Search..." class="search-input" />
+  </div>
+  
+  <style>
+    .search-container {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      max-width: 500px;
+      background-color: var(--c_grey);
+      border: none;
+      border-radius: 25px;
+      overflow: hidden;
+      padding: 1px 9px;
+    }
+  
+    .search-input {
+      width: 100%;
+      height: 40px;
+      border: none;
+      outline: none;
+      background-color: transparent;
+      padding-left: 10px;
+      color: var(--c_grey_dark);
+    }
+  
+    @media (max-width: 768px) {
+      .search-container {
+        max-width: 100%;
+      }
+    }
+  </style>
+  
