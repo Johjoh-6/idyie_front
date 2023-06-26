26731e9fd55875ab32e0e0dc9b1c3580136a8833
Johann six <six.johann@gmail.com>
2023-06-26 19:41:16 +0200

    ✨ add first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Home</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/login">Login</a>
+<a href="/register">register</a>
