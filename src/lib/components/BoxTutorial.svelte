e79d456bccd6f788a740e094e48ace91a5d08916
Johann six <six.johann@gmail.com>
2023-06-27 11:03:19 +0200

    🚧 on box tutorial

--- src/lib/components/BoxTutorial.svelte
+++ src/lib/components/BoxTutorial.svelte
@@ -1,31 +1,36 @@
 <script lang='ts'>
+  import type { Tutorial } from "$lib/model/tutorial";
 
+
+    export let tuto : Tutorial;
 </script>
 
+<a href="/{tuto.id}">
 <div class="box">
     <div class="user_info">
-        <img src="https://avatars.githubusercontent.com/u/59853942?v=4" alt="user">
+        <img src="{tuto.user.avatar ?? ""}" alt="user">
         <div class="user_name">
-            <p>username</p>
+            <p>{tuto.user.username}</p>
         </div>
         <div class="info_tuto">
             <div class="categorie">
-
+                {tuto.categorie.name}
             </div>
-            <div class="rating"></div>
+            <div class="rating">{tuto.avg_rating}</div>
             <div class="comment"></div>
         </div>
         <div class="tutorial">
-                <h2>title</h2>
+                <h2>{tuto.title}</h2>
            
             <div class="content">
-                content
+                <p>{@html tuto.content}</p>
             </div>
-            <p class="date">date</p>
+            <p class="date">{tuto.date}</p>
         </div>
     </div>
     
 </div>
+</a>
 
 <style>
     .box{
