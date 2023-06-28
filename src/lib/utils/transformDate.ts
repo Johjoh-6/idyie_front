a4e79ad91aeade0777fe40a3a852425f2e7e5ee7
Johann six <six.johann@gmail.com>
2023-06-28 14:19:36 +0200

    🎨 update transform data

--- src/lib/utils/transformDate.ts
+++ src/lib/utils/transformDate.ts
@@ -1,7 +1,11 @@
 
 
-export const transformDate = (date: string) => {
+export const transformDate = (date: string): {date: string, time:string} => {
     const dateObj = new Date(date);
     const local = dateObj.toLocaleString('fr-FR');
-    return local;
+    const dateArray = local.split(' ');
+    return {
+        date: dateArray[0],
+        time: dateArray[1]
+    };
 }   
