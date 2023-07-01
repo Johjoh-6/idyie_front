d683090569914c909c83c708127f82bfa0220a27
Johann six <six.johann@gmail.com>
2023-07-01 10:08:43 +0200

    🚧 add admin group

---
+++ src/routes/(authed)/(admin)/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+
+<svelte:head>
+    <title>Administré Idyie</title>
+</svelte:head>
