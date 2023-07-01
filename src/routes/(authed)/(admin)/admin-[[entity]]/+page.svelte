f8387e69436fe45600cf7115722562dc421c103a
Johann six <six.johann@gmail.com>
2023-07-01 19:58:53 +0200

    🚧 need to add a dashboard

--- src/routes/(authed)/(admin)/admin-[[entity]]/+page.svelte
+++ src/routes/(authed)/(admin)/admin-[[entity]]/+page.svelte
@@ -7,4 +7,18 @@ import type { PageData } from './$types';
 
 <h1>{data.entity}</h1>
 
-<Table data={data.dataList}  />
+{#if data.entity !== "dashboard"}
+    <Table data={data.dataList}  actionDelete={"?/delete"} watch={`admin-${data.entity}/see-`} edit={`admin-${data.entity}/edit-`}/>
+{:else}
+    <p>test</p>
+{/if}
+
+<style lang="scss">
+    h1 {
+        text-align: center;
+        text-transform: capitalize;
+        font-size: 2rem;
+        color: var(--c_primary);
+        margin-bottom: 1rem;
+    }
+</style>
