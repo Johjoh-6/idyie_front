f7e19dde2bfb36499e54d1e2fd533fd304ec92c8
Johann six <six.johann@gmail.com>
2023-07-01 19:57:22 +0200

    ✨ add see entity by id

---
+++ src/routes/(authed)/(admin)/admin-[[entity]]/see-[id]/+page.server.ts
@@ -0,0 +1,38 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { getTutorial } from '$lib/servers/tutorial';
+import { getCategory } from '$lib/servers/category';
+import { getComment } from '$lib/servers/comments';
+import { getUser } from '$lib/servers/user';
+
+export const load = (async ({params}) => {
+    const { entity, id } = params;
+    if(!entity) {
+        throw error(404, 'Entité non trouvée');
+    }
+    if(!id) {
+        throw error(404, 'ID non trouvé');
+    }
+
+    let selected;
+    switch(entity) {
+        case "users":
+            selected = await getUser(parseInt(id));
+            break;
+        case "tutoriels":
+            selected = await getTutorial(parseInt(id));
+            break;
+        case "categorie":
+            selected = await getCategory(parseInt(id));
+            break;
+        case "comments":
+            selected = await getComment(parseInt(id));
+            break;
+        default:
+            throw error(404, 'Entité non trouvée');
+    }
+
+    return {
+        selected,
+    };
+}) satisfies PageServerLoad;
---
+++ src/routes/(authed)/(admin)/admin-[[entity]]/see-[id]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+    console.log(data.selected);
+</script>
+
+<!-- render all key and value in the data.selected -->
+{#each Object.entries(data.selected) as [key, value]}
+    <!-- Check if value is an object -->
+    {#if typeof value === "object"}
+    <div>
+        <!-- render all key and value in the value -->
+        {#each Object.entries(value) as [key, v]}
+            <p>{key} : {v}</p>
+        {/each}
+    </div>
+    {:else}
+    <p>{key} : {value}</p>
+    {/if}
+{/each}
+
+
+<style lang="scss">
+    div{
+        border: 1px solid var(--c_primary);
+        padding: 1rem;
+        margin-bottom: 1rem;
+    }
+    p{
+        margin-bottom: 0.5rem;
+        text-transform: capitalize;
+    }
+</style>
