3c93e407b0e6d378f6447491c16688f6092f832b
Johann six <six.johann@gmail.com>
2023-07-01 10:09:40 +0200

    ✨ add profile page and list of tutorial

---
+++ src/routes/(authed)/profile/+page.server.ts
@@ -0,0 +1,63 @@
+import { error, fail, json } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { deleteTutorial, getTutorialByUser } from '$lib/servers/tutorial';
+import { getUserById, updateUser } from '$lib/servers/user';
+
+export const load: PageServerLoad = async ({ locals }) => {
+    if(!locals.user) throw error(403, "Vous n'avez pas les droits pour accéder à cette page.");
+    const id = locals.user.id;
+    const tutorial = await getTutorialByUser(id);
+    const fullUser = await getUserById();
+    console.log(fullUser);
+    
+	return {
+		user: fullUser,
+        tutoUser: tutorial
+	};
+};
+
+export const actions: Actions = {
+    update: async ({ request, locals }) => {
+        const data = await request.formData();
+        const l_name = data.get('l_name') as string;
+        const f_name = data.get('f_name') as string;
+        const email = data.get('email') as string;
+        const password = data.get('password') as string;
+        const passwordConfirm = data.get('passwordConfirm') as string;
+        const username = data.get('username') as string;
+        const avatar = data.get('avatar') as string;
+
+        let passwordVerify = "";
+        if(password !== passwordConfirm) {
+            return fail(400, { message: "Les mots de passe ne correspondent pas." });
+        }else{
+            if(password.length > 0 && password.length < 8) { 
+                return fail(400, { message: "Le mot de passe doit contenir au moins 8 caractères." });
+            } else {
+                passwordVerify = password;
+            }
+        }
+
+
+        const user = {
+            l_name: l_name,
+            f_name: f_name,
+            email: email,
+            password: passwordVerify,
+            username: username,
+            avatar: avatar
+        };
+
+        const res = await updateUser(user);
+        if (res.username){
+            return json({success: "Votre compte a bien été modifié"});
+        } 
+
+    },
+    delete: async ({ request}) => {
+        const data = await request.formData();
+        const id = data.get('id') as string;
+        const res = await deleteTutorial(parseInt(id));
+        console.log(res);
+    }
+};
---
+++ src/routes/(authed)/profile/+page.svelte
@@ -0,0 +1,211 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import { transformDate } from '$lib/utils/transformDate';
+	import type { PageData, ActionData } from './$types';
+
+	export let data: PageData;
+    export let form: ActionData;
+
+    const dateChange = (created : string ) => {
+        const {date, time } =transformDate(created);
+        return `Le ${date} à ${time}`;
+    }
+
+</script>
+
+<svelte:head>
+    <title>Mon profil</title>
+</svelte:head>
+
+<h1>Bienvenue {data.user?.username ?? 'Inconnue'}</h1>
+
+<section class="update_profile">
+    <h2>Mettre a jour votre profile</h2>
+    {#if form?.message}
+        <p class="error">{form.message}</p>
+    {/if}
+    
+    <form  action="?/update" method="POST" use:enhance>
+        <label for="l_name">Nom</label>
+        <input name="l_name" type="text" value={data.user?.l_name ?? 'Inconnue'} />
+
+        <label for="f_name">Prénom</label>
+        <input name="f_name" type="text" value={data.user?.f_name ?? 'Inconnue'} />
+
+        <label for="username">Username</label>
+        <input name="username" type="text" value={data.user?.username ?? 'Inconnue'} />   
+    
+        <label for="email">Email</label>
+        <input name="email" type="email" placeholder="email" value="{data.user?.email}" />
+
+        <label for="password">Mot de passe</label>
+        <input name="password" type="password" placeholder="password" />
+
+        <label for="passwordConfirm">Confirmer le mot de passe</label>
+        <input name="passwordConfirm" type="password" placeholder="password" />
+
+        <label for="avatar">Avatar</label>
+        <input name="avatar" type="url" value={data.user?.avatar}/>
+
+
+
+        <button class="btn btn-outline-primary">Update</button>
+    </form>
+</section>
+
+{#if data.user?.role !== "user" }
+<section class="tutorials">
+    {#if data.tutoUser.length > 0}
+        <h2>Vos tutoriels</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Titre</th>
+                    <th>Catégorie</th>
+                    <th>Durée</th>
+                    <th>Date</th>
+                    <th>Voir</th>
+                    <th>Éditer</th>
+                    <th>Supprimer</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each data.tutoUser as tutoriel}
+                    <tr>
+                        <td data-cell="titre">{tutoriel.title}</td>
+                        <td data-cell="Catégorie">{tutoriel.categorie.name}</td>
+                        <td data-cell="Durée">{tutoriel.durate} min</td>
+                        <td data-cell="Date">{dateChange(tutoriel.created_at)}</td>
+                        <td data-cell="Voir"><a href="/{tutoriel.id}">Voir</a></td>
+                        <td data-cell="Éditer"><a href="/edit-tutoriel-{tutoriel.id}">Editer</a></td>
+                        <td data-cell="Supprimer"><form action="/profile?/delete" method="POST" use:enhance>
+                            <input type="hidden" name="id" value={tutoriel.id} />
+                            <button class="btn btn-outline-danger">Supprimer</button>
+                        </form></td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    {:else}
+        <h2>Vous n'avez pas encore de tutoriel</h2>
+    {/if}
+</section>
+{/if}
+
+<style lang="scss">
+
+h1{
+    margin-bottom: 1rem;
+    padding: 1rem;
+    text-align: center;
+    color: var(--c_primary);
+    border-bottom: var(--c_primary) 3px solid;
+}
+.update_profile{
+    padding: 2rem 0;
+    border-bottom: var(--c_primary) 3px solid;
+    h2{
+        margin-bottom: 1rem;
+        text-align: center;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        label{
+            margin-bottom: 0.5rem;
+            color: var(--c_primary);
+        }
+        input{
+            margin-bottom: 1rem;
+            padding: 0.5rem 1rem;
+            border-radius: var(--br_m);
+            border: 1px solid var(--c_primary);
+        }
+        button{
+            border: none;
+            padding: 0.5rem 1rem;
+            border-radius: var(--br_m);
+            cursor: pointer;
+            background-color: var(--c_primary);
+            color: var(--c_white);
+        }
+    }
+}
+
+.tutorials{
+    padding: 1rem 0 ;
+    h2{
+        margin-bottom: 1rem;
+        text-align: center;
+        color: var(--c_primary);
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 0.3rem;
+        form{
+            display: inline-block;
+        }
+        button{
+        border: none;
+        padding: 0.5rem 1rem;
+        border-radius: var(--br_m);
+        cursor: pointer;
+        background-color: var(--c_red);
+        color: var(--c_white);
+    }
+    }
+    th{
+        background-color: var(--c_primary);
+        color: var(--c_white);
+    }
+    td{
+        text-align: center;
+    }
+    td::before{
+        display: none;
+    }
+
+    tr:nth-child(even) {
+        background-color: var(--box_light);
+    }
+    
+    @media screen and (max-width: 600px) {
+        td::before{
+            display: inline-block;
+            content: attr(data-cell) " : ";
+            text-transform: capitalize;
+            font-weight: bold;
+            margin-right: 5px;
+            color: var(--c_primary);
+        }
+        td{
+            text-align: left;
+            display: block;
+            padding: 0.5rem 1rem;
+        }
+        th{
+            display: none;
+        }
+        td:first-child{
+            border-top-left-radius: var(--br_sm);
+            border-top-right-radius: var(--br_sm);
+            padding-top: 2rem;
+        }
+        td:last-child{
+            border-bottom-left-radius: var(--br_sm);
+            border-bottom-right-radius: var(--br_sm);
+            padding-bottom: 2rem;
+        }
+        tr{
+            border: 1px solid var(--box_light);
+        }
+        tr:nth-child(even) {
+            background-color: var(--box_light);
+        }
+    }
+}
+</style>
