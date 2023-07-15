2e16d22c191f9eb826ab49abfd67e03288031375
Johann six <six.johann@gmail.com>
2023-07-15 12:51:16 +0200

    🎨 return better ui and information

--- src/routes/(authed)/(admin)/admin-[[entity]]/see-[id]/+page.svelte
+++ src/routes/(authed)/(admin)/admin-[[entity]]/see-[id]/+page.svelte
@@ -1,36 +1,77 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
     import type { PageData } from './$types';
     
     export let data: PageData;
 </script>
 
-<!-- render all key and value in the data.selected -->
-{#each Object.entries(data.selected) as [key, value]}
-    <!-- Check if value is an object -->
-    {#if typeof value === "object"}
-    <div>
-        <!-- render all key and value in the value -->
-        {#each Object.entries(value) as [key, v]}
-            <p>{key} : {v}</p>
-        {/each}
-    </div>
-    {:else}
-    <p>{key} : {value}</p>
-    {/if}
-{/each}
 
+<section>
+{#each Object.entries(data.selected) as [key, value]}
+{#if key !== "password" && key !== "created_at" && key !== "updated_at" && key !== "date" && key !== "avg_rating" && key !== "user" && key !== "id_category_parent" && key !== "tutorial"}
+            {#if key === "role" }
+                <p><span class="title"> Role : </span>{value}</p>
+            {:else if key === "ban"}
+                <p><span class="title">Ban : </span>{value ? "Vrai" : "faux"}</p>
+            {:else if key === "email"}
+               <p><span class="title">Email : </span><a href="mailto:{value}">{value}</a></p>
+            {:else if key === "avatar"}
+                <img src="{value}" alt="avatar">
+            {:else if key === "content"}
+                <p><span class="title">Contenue : </span></p>
+                {@html value}
+            {:else if key === "view_count"}
+                <p><span class="title">Nombre de vue :</span> {value}</p>
+            {:else if key ==="durate"}
+                <p><span class="title">Durée : </span>{value}</p>
+            {:else if key === "categorie"}
+                <p><span class="title">Catégorie : </span>{value.name}</p>
+            {:else if key === "draft"}
+                <p><span class="title">Publiée :</span> {value ? "Publié": "Brouillon"}</p>
+            {:else}
+                <p><span class="title">{key} :</span> {value}</p>
+            {/if}
+        {/if}
+{/each}
+<button on:click={() => {
+    if ('id' in data.selected) {
+        goto(`edit-${data.selected.id}`);
+    }
+}}>Modifier</button>
+</section>
 
 <style lang="scss">
-    div{
-        border: 1px solid var(--c_primary);
-        padding: 1rem;
-        margin-bottom: 1rem;
+
+    section{
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    span{
+        font-size: clamp(1rem, 2vw, 1.5rem);
+        color: var(--c_primary);
+        font-weight: bold;
     }
-    p{
-        margin-bottom: 0.5rem;
-        text-transform: capitalize;
-        max-width: 100%;
-        display: flex;
-        word-break: break-all;
+    img{
+        width: 150px;
+        height: auto;
+        border-radius: var(--br_round);
+    }
+    }
+    button{
+        padding: 10px;
+        border-radius: var(--br_sm);
+        color: var(--text_light);
+        background-color: var(--c_grey);
+        border: none;
+        font-weight: bold;
+        font-size: clamp(1rem, 2vw, 1.5rem);
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+        &:hover{
+            background-color: var(--c_primary);
+            color: var(--c_grey);
+        }
     }
 </style>
