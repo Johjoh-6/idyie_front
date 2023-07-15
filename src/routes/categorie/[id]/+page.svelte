9f86f178644a6ffb62cdfddd29cb282be8740fbd
Johann six <six.johann@gmail.com>
2023-07-15 12:51:43 +0200

    💄 improve style

--- src/routes/categorie/[id]/+page.svelte
+++ src/routes/categorie/[id]/+page.svelte
@@ -6,7 +6,23 @@ import type { PageData } from './$types';
 </script>
 
 <h1>{data.category.name}</h1>
-
-{#each data.tutorials as tuto}
+<section>
+    {#each data.tutorials as tuto}
     <BoxTutorial {tuto} />
-{/each}
+    {/each}
+</section>
+    
+<style>
+h1{
+        text-align: center;
+        font-size: 2rem;
+        color: var(--c_primary);
+        margin-bottom: 1rem;
+    }
+     section{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+        grid-gap: 1rem;
+        padding: 10px;
+    }
+</style>
